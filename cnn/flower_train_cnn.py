11038131ea2f4cb28241a6dbd0833b956ea77feb
michelevantini <vantinimichele95@gmail.com>
2018-01-09 16:40:57 +0200

    seq reading working

--- cnn/flower_train_cnn.py
+++ cnn/flower_train_cnn.py
@@ -4,8 +4,11 @@ import os # path join
 
 
 DATA_DIR = "../dataset/"
-TRAINING_SET_SIZE = 1000
-BATCH_SIZE = 50
+TRAIN_FILE = "train.tfrecord"
+VALIDATION_FILE = "validation.tfrecord"
+TRAINING_SET_SIZE = 3200
+N_EPOCHS = 5
+BATCH_SIZE = 10
 IMAGE_SIZE = 1536
 N_CHANNEL = 3
 N_CLASSES = 4
@@ -25,60 +28,59 @@ def _int64_feature(value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-# image object from protobuf
-class _image_object:
-    def __init__(self):
-        self.image = tf.Variable([], dtype = tf.string)
-        self.height = tf.Variable([], dtype = tf.int64)
-        self.width = tf.Variable([], dtype = tf.int64)
-        self.filename = tf.Variable([], dtype = tf.string)
-        self.label = tf.Variable([], dtype = tf.int32)
 
-def read_and_decode(filename_queue):
-    reader = tf.TFRecordReader()
-    _, serialized_example = reader.read(filename_queue)
+def decode(serialized_example):
     features = tf.parse_single_example(serialized_example, features=FEATURES_LIST)
     image_encoded = features["image/encoded"]
     image_raw = tf.image.decode_jpeg(image_encoded, channels=N_CHANNEL)
-    image_object = _image_object()
-    image_object.image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
-    image_object.height = features["image/height"]
-    image_object.width = features["image/width"]
-    image_object.filename = features["image/filename"]
-    image_object.label = tf.cast(features["image/class/label"], tf.int64)
-    return image_object
+    #image_object = _image_object()
+    image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
+    #image_object.image = image
+    #image_object.height = features["image/height"]
+    #image_object.width = features["image/width"]
+    #image_object.filename = features["image/filename"]
+    label = tf.cast(features["image/class/label"], tf.int64)
+    #image_object.label = label
+    return image, label
 
-def input_fn(filenames, if_random = True):
-    for f in filenames:
-        if not tf.gfile.Exists(f):
-            raise ValueError("Failed to find file: " + f)
-    filename_queue = tf.train.string_input_producer(filenames)
-    image_object = read_and_decode(filename_queue)
-    image = tf.image.per_image_standardization(image_object.image)
-#    image = image_object.image
-#    image = tf.image.adjust_gamma(tf.cast(image_object.image, tf.float32), gamma=1, gain=1) # Scale image to (0, 1)
-    label = image_object.label
-    filename = image_object.filename
+def inputs(train, batch_size, num_epochs):
+  """Reads input data num_epochs times.
+  Args:
+    train: Selects between the training (True) and validation (False) data.
+    batch_size: Number of examples per returned batch.
+    num_epochs: Number of times to read the input data, or 0/None to
+       train forever.
+  Returns:
+    A tuple (images, labels), where:
+    * images is a float tensor with shape [batch_size, mnist.IMAGE_PIXELS]
+      in the range [-0.5, 0.5].
+    * labels is an int32 tensor with shape [batch_size] with the true label,
+      a number in the range [0, mnist.NUM_CLASSES).
+    This function creates a one_shot_iterator, meaning that it will only iterate
+    over the dataset once. On the other hand there is no special initialization
+    required.
+  """
+  if not num_epochs: num_epochs = None
+  filename = os.path.join(DATA_DIR,
+                          TRAIN_FILE if train else VALIDATION_FILE)
 
-    if(if_random):
-        min_fraction_of_examples_in_queue = 0.4
-        min_queue_examples = int(TRAINING_SET_SIZE * min_fraction_of_examples_in_queue)
-        print("Filling queue with %d images before starting to train. " "This will take a few minutes." % min_queue_examples)
-        num_preprocess_threads = 1
-        image_batch, label_batch, filename_batch = tf.train.shuffle_batch(
-            [image, label, filename],
-            batch_size = BATCH_SIZE,
-            num_threads = num_preprocess_threads,
-            capacity = min_queue_examples + 3 * BATCH_SIZE,
-            min_after_dequeue = min_queue_examples)
-        return image_batch, label_batch, filename_batch
-    else:
-        image_batch, label_batch, filename_batch = tf.train.batch(
-            [image, label, filename],
-            batch_size = BATCH_SIZE,
-            num_threads = 1)
-        return image_batch, label_batch, filename_batch
+  with tf.name_scope('input'):
+    # TFRecordDataset opens a protobuf and reads entries line by line
+    # could also be [list, of, filenames]
+    dataset = tf.data.TFRecordDataset(filename)
+    dataset = dataset.repeat(num_epochs)
 
+    # map takes a python function and applies it to every sample
+    dataset = dataset.map(decode)
+    #dataset = dataset.map(augment)
+    #dataset = dataset.map(normalize)
+
+    #the parameter is the queue size
+    dataset = dataset.shuffle(2 * batch_size)
+    dataset = dataset.batch(batch_size)
+
+    iterator = dataset.make_one_shot_iterator()
+  return iterator.get_next()
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.05)
@@ -106,57 +108,63 @@ def inference_fn(image_batch):
 
 
 def train_fn():
-    image_batch_out, label_batch_out, filename_batch = input_fn([DATA_DIR + "train.tfrecord"])
+    with tf.Graph().as_default():
+        image_batch, label_batch = inputs(train=True, batch_size = BATCH_SIZE
+                                                        , num_epochs=N_EPOCHS)
 
-    image_batch_placeholder = tf.placeholder(tf.float32
-                            , shape=[BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL])
-    image_batch = tf.reshape(image_batch_out
-                            , (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL))
+        image_batch_placeholder = tf.placeholder(tf.float32
+                                , shape=[BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL])
+        image_batch = tf.reshape(image_batch
+                                , (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL))
 
-    label_batch_placeholder = tf.placeholder(tf.float32, shape=[BATCH_SIZE, N_CLASSES])
-    label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
-    label_batch_one_hot = tf.one_hot(tf.add(label_batch_out, label_offset)
-                                     , depth=N_CLASSES, on_value=1.0, off_value=0.0)
+        label_batch_placeholder = tf.placeholder(tf.float32, shape=[BATCH_SIZE, N_CLASSES])
+        label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
+        label_batch_one_hot = tf.one_hot(tf.add(label_batch, label_offset)
+                                         , depth=N_CLASSES, on_value=1.0, off_value=0.0)
 
-    logits_out = inference_fn(image_batch_placeholder)
-#    loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_one_hot, logits=logits_out))
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_placeholder, logits=logits_out))
+        logits = inference_fn(image_batch_placeholder)
+    #    loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_one_hot, logits=logits))
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_placeholder, logits=logits))
 
-    train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(loss)
+        train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(loss)
 
-    #saver = tf.train.Saver()
+        #saver = tf.train.Saver()
 
-    with tf.Session() as sess:
-        # Visualize the graph through tensorboard.
-        file_writer = tf.summary.FileWriter("./logs", sess.graph)
+        with tf.Session() as sess:
+            # Visualize the graph through tensorboard.
+            file_writer = tf.summary.FileWriter("./logs", sess.graph)
 
-        sess.run(tf.global_variables_initializer())
-        #saver.restore(sess, "dltmp/checkpoint-train.ckpt")
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+            sess.run(tf.global_variables_initializer())
+            #saver.restore(sess, "dltmp/checkpoint-train.ckpt")
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord, sess=sess)
 
-        for i in range(500):
-            image_out, label_out, label_batch_one_hot_out, filename_out = sess.run([image_batch, label_batch_out, label_batch_one_hot, filename_batch])
+            try:
+                step = 0
+                while True:
+                    image_out, label_out, label_batch_one_hot_out = sess.run([image_batch, label_batch, label_batch_one_hot])
 
-            _, infer_out, loss_out = sess.run([train_step, logits_out, loss], feed_dict={image_batch_placeholder: image_out, label_batch_placeholder: label_batch_one_hot_out})
+                    _, infer_out, loss_out = sess.run([train_step, logits, loss], feed_dict={image_batch_placeholder: image_out, label_batch_placeholder: label_batch_one_hot_out})
 
-            if i%10 == 0:
-                print(i)
-                print(image_out.shape)
-                print("label_out: ")
-                print(filename_out)
-                print(label_out)
-                print(label_batch_one_hot_out)
-                print("infer_out: ")
-                print(infer_out)
-                print("loss: ")
-                print(loss_out)
-            #if(i%50 == 0):
-            #    saver.save(sess, "dltmp/checkpoint-train.ckpt")
+                    if step%10 == 0:
+                        print(step)
+                        #print(image_out.shape)
+                        #print("label_out: ")
+                        #print(filename_out)
+                        #print(label_out)
+                        #print(label_batch_one_hot_out)
+                        #print("infer_out: ")
+                        #print(infer_out)
+                        print("loss: ", loss_out)
+                    #if(i%50 == 0):
+                    #    saver.save(sess, "dltmp/checkpoint-train.ckpt")
+                    step +=1
+            except tf.errors.OutOfRangeError:
+                print('Done training for %d epochs, %d steps.' % (N_EPOCHS, step))
 
-        coord.request_stop()
-        coord.join(threads)
-        sess.close()
+            coord.request_stop()
+            coord.join(threads)
+            sess.close()
 
 
 '''
@@ -206,5 +214,7 @@ def flower_eval():
         coord.join(threads)
         sess.close()
 '''
-train_fn()
-#flower_eval()
+
+if __name__ == '__main__':
+    train_fn()
+    #flower_eval()
