dfba04b28f488dcf76c62f495528ef7150c8a814
Fabio Colella <fcole90@gmail.com>
2018-01-10 21:53:35 +0200

    Added regularization, added more tunable variables, configured multiCPU, changed to leakyRELU, changed to Adam optimiser, improved output format

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -17,12 +17,16 @@ assert os.path.exists(os.path.join(DATA_DIR, TRAIN_FILE))
 # assert os.path.exists(DATA_DIR, VALIDATION_FILE)
 TRAINING_SET_SIZE = 3200
 N_EPOCHS = 5
-BATCH_SIZE = 10
+BATCH_SIZE = 8  # using a power of 2
+QUEUE_SIZE = 2
 IMAGE_SIZE = 1536
-N_CHANNEL = 3
+N_CHANNELS = 3
 N_CLASSES = 4
 FILTERS_CHOICES_LAYER_1 = [2, 3, 5, 4, 6, 10, 7]
 LEARNING_RATE = 0.000001
+CNN_REGULARIZATION = 0.0002
+FC_REGULARIZATION = 0.0002
+PRINT_RESOLUTION = 1  # print an update every x iterations
 NETWORK_CHOICE = "test_1"  # choices: "default", "original", "test_1"
 
 FEATURES_LIST = {"image/encoded": tf.FixedLenFeature([], tf.string),
@@ -43,7 +47,7 @@ def _bytes_feature(value):
 def decode(serialized_example):
     features = tf.parse_single_example(serialized_example, features=FEATURES_LIST)
     image_encoded = features["image/encoded"]
-    image_raw = tf.image.decode_jpeg(image_encoded, channels=N_CHANNEL)
+    image_raw = tf.image.decode_jpeg(image_encoded, channels=N_CHANNELS)
     # image_object = _image_object()
     image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
     # image_object.image = image
@@ -57,12 +61,15 @@ def decode(serialized_example):
 
 def inputs(train, batch_size, num_epochs):
     """Reads input data num_epochs times.
-    Args:
-      train: Selects between the training (True) and validation (False) data.
-      batch_size: Number of examples per returned batch.
-      num_epochs: Number of times to read the input data, or 0/None to
-         train forever.
-    Returns:
+
+    Parameters
+    ----------
+        train: Selects between the training (True) and validation (False) data.
+        batch_size: Number of examples per returned batch.
+        num_epochs: Number of times to read the input data, or 0/None to train forever.
+
+    Returns
+    -------
         A tuple (images, labels), where:
 
         * images is a float tensor with shape [batch_size, mnist.IMAGE_PIXELS]
@@ -74,7 +81,9 @@ def inputs(train, batch_size, num_epochs):
         over the dataset once. On the other hand there is no special initialization
         required.
     """
-    if not num_epochs: num_epochs = None
+    if not num_epochs:
+        num_epochs = None
+
     filename = os.path.join(DATA_DIR,
                             TRAIN_FILE if train else VALIDATION_FILE)
 
@@ -90,29 +99,11 @@ def inputs(train, batch_size, num_epochs):
         # dataset = dataset.map(normalize)
 
         # the parameter is the queue size
-        dataset = dataset.shuffle(2 * batch_size)
+        dataset = dataset.shuffle(QUEUE_SIZE * batch_size)
         dataset = dataset.batch(batch_size)
 
         iterator = dataset.make_one_shot_iterator()
     return iterator.get_next()
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.05)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.02, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 def generate_cnn(image_batch, n_filters, filters_sizes, pool_sizes, reuse, flatten=True):
@@ -121,16 +112,18 @@ def generate_cnn(image_batch, n_filters, filters_sizes, pool_sizes, reuse, flatt
     assert len(n_filters) == len(filters_sizes) == len(pool_sizes)
 
     with tf.variable_scope('ConvNet', reuse=reuse):
-        conv_input = tf.reshape(image_batch, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL])
+        conv_input = tf.reshape(image_batch, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS])
         pool = None
         after_pool_size = IMAGE_SIZE
+        regularizer = tf.contrib.layers.l2_regularizer(scale=CNN_REGULARIZATION)
         for i in range(len(n_filters)):
             conv = tf.layers.conv2d(
                 inputs=conv_input,
                 filters=n_filters[i],
                 kernel_size=filters_sizes[i],
                 strides=(1, 1),
-                activation=tf.nn.relu
+                activation=tf.nn.leaky_relu,
+                kernel_regularizer=regularizer
             )
 
             # Compute size of following layer.
@@ -158,23 +151,42 @@ def generate_cnn(image_batch, n_filters, filters_sizes, pool_sizes, reuse, flatt
 
 
 def generate_fc(fc_input, layer_sizes):
+    """Creates a regularised fully connected network.
+
+    Parameters
+    ----------
+    fc_input: Tensor input
+        Input layer of the network.
+    layer_sizes: list of int
+        List of sizes for the layers of the network.
+
+    Returns
+    -------
+    Output tensor.
+        A tensorflow tensor representing the network graph.
+
+    """
     for i in range(len(layer_sizes)):
+        regularizer = tf.contrib.layers.l2_regularizer(scale=FC_REGULARIZATION)
         fc_input = tf.layers.dense(
             inputs=fc_input,
-            units=layer_sizes[i]
+            units=layer_sizes[i],
+            activation=tf.nn.leaky_relu,
+            kernel_regularizer=regularizer
         )
 
     return fc_input
 
 
 def train_fn():
+    """Function to run the train session."""
     with tf.Graph().as_default():
         image_batch, label_batch = inputs(train=True, batch_size=BATCH_SIZE,
                                           num_epochs=N_EPOCHS)
 
         image_batch_placeholder = tf.placeholder(tf.float32,
-                                                 shape=[BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL])
-        image_batch = tf.reshape(image_batch, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNEL))
+                                                 shape=[BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS])
+        image_batch = tf.reshape(image_batch, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS))
 
         label_batch_placeholder = tf.placeholder(tf.float32, shape=[BATCH_SIZE, N_CLASSES])
         label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
@@ -194,7 +206,7 @@ def train_fn():
                 n_filters=[16, 32, 64, 64, 32],
                 filters_sizes=[[3, 3], [3, 3], [3, 3], [3, 3], [3, 3]],
                 pool_sizes=[[3, 3], [2, 2], [2, 2], [3, 3], [3, 3]],
-                reuse=False,
+                reuse=tf.AUTO_REUSE,
                 flatten=True
             )
 
@@ -204,19 +216,19 @@ def train_fn():
             )
 
         # --- Network for testing configuration 1 ---
-        if NETWORK_CHOICE == "test_1":
+        elif NETWORK_CHOICE == "test_1":
             network_cnn = generate_cnn(
                 image_batch=image_batch_placeholder,
                 n_filters=[64, 32, 64, 32, 64, 32],
                 filters_sizes=[[2, 2], [3, 3], [3, 3], [3, 3], [3, 3], [3, 3]],
                 pool_sizes=[[3, 3], [3, 3], [2, 2], [2, 2], [3, 3], [3, 3]],
-                reuse=False,
+                reuse=tf.AUTO_REUSE,
                 flatten=True
             )
 
             network_fc = generate_fc(
                 fc_input=network_cnn,
-                layer_sizes=[512, 256, 128, 64, 16, N_CLASSES]
+                layer_sizes=[2048, 512, 256, 64, 16, N_CLASSES]
             )
 
         # --- Default network ---
@@ -238,14 +250,20 @@ def train_fn():
                 layer_sizes=[N_CLASSES]
             )
 
-        # loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_one_hot, network_cnn=network_cnn))
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_placeholder, logits=network_fc))
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=label_batch_placeholder,
+                                               logits=network_fc)
+        regularisation_loss = tf.losses.get_regularization_loss()
+        loss += regularisation_loss
 
         train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
 
         # saver = tf.train.Saver()
 
-        with tf.Session() as sess:
+        config = tf.ConfigProto(device_count={"CPU": 12},
+                                inter_op_parallelism_threads=1,
+                                intra_op_parallelism_threads=1)
+
+        with tf.Session(config=config) as sess:
             print("Starting training.. Using {} network architecture.".format(NETWORK_CHOICE))
             # Visualize the graph through tensorboard.
             # file_writer = tf.summary.FileWriter("./logs", sess.graph)
@@ -258,6 +276,7 @@ def train_fn():
 
             # Start taking time (full)
             start_time = time.time()
+            old_loss = np.Inf
 
             try:
                 while True:
@@ -273,11 +292,13 @@ def train_fn():
                     # Save time (iteration)
                     iteration_time = time.time() - start_time_iteration
 
-                    if step % 10 == 0:
+                    if step % PRINT_RESOLUTION == 0:
                         print(
                             step, ": ", "loss: %.3f " % loss_out,
-                            " - iteration time: %.2fs" % iteration_time,
+                            "- iteration time: %.2fs" % iteration_time,
+                            " IMPROVING" if loss_out < old_loss else "",
                             sep="")
+                        old_loss = loss_out
                         # print(image_out.shape)
                         # print("label_out: ")
                         # print(filename_out)
